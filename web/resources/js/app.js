e7696d6c311b203163f3afb56995be3c12024c70
Shion Aoki <shio0321pc@gmail.com>
2020-12-13 15:59:22 +0900

    add : vuerouter追加による修正

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  template: '<h1>Hello world</h1>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App />'
 })
