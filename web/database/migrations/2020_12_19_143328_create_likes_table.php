77a1500a06bce63e404a5c41f14f40ed19b6cd99
Shion Aoki <shio0321pc@gmail.com>
2020-12-19 23:50:04 +0900

    add : migration files

--- web/database/migrations/2020_12_19_143328_create_likes_table.php
+++ web/database/migrations/2020_12_19_143328_create_likes_table.php
@@ -14,8 +14,13 @@ class CreateLikesTable extends Migration
     public function up()
     {
         Schema::create('likes', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('photo_id');
+            $table->unsignedInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('photo_id')->references('id')->on('photos');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
